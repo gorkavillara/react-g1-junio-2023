45bb3ce758518312f2c7d393783d1502c8bbf3a2
gorkavillar <gorka@gorkavillar.com>
2023-06-29 12:24:43 +0200

    Tema 7 - Formularios clásicos y con Formik y yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.scss"
-// import * as views from "./views"
-// import Views from "./views"
-import { EstilosCondicionales, Alumnos } from "./views"
+import { FormularioFormik } from "./views"
 function App() {
     return (
         <>
-            {/* <Principal /> */}
-            <Alumnos />
-            <EstilosCondicionales />
+            <FormularioFormik />
         </>
     )
 }
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,7 +1,16 @@
-import Principal from "./Principal/Principal";
-import Estilos from "./Estilos/Estilos";
-import EstilosCondicionales from "./EstilosCondicionales/EstilosCondicionales";
-import Alumnos from "./Alumnos/Alumnos";
+import Principal from "./Principal/Principal"
+import Estilos from "./Estilos/Estilos"
+import EstilosCondicionales from "./EstilosCondicionales/EstilosCondicionales"
+import Alumnos from "./Alumnos/Alumnos"
+import FormularioClasico from "./Formularios/FormularioClasico"
+import FormularioFormik from "./Formularios/FormularioFormik"
 
-export { Principal, Estilos, EstilosCondicionales, Alumnos }
+export {
+    Principal,
+    Estilos,
+    EstilosCondicionales,
+    Alumnos,
+    FormularioClasico,
+    FormularioFormik,
+}
 // export default { Principal, Estilos }
