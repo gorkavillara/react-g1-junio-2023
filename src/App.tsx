c92ad5bf6d9d1682849f2de8819951a1471a9fb6
gorkavillar <gorka@gorkavillar.com>
2023-07-13 12:13:47 +0200

    Tema 13 - Build y variables de entorno

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import "./App.scss"
 import Contador from "./views/Contador/Contador"
 
 function App() {
+    const mode = import.meta.env.MODE
     return (
-        <>
+        <div className={mode}>
+            <h2>{import.meta.env.VITE_APP_NAME}</h2>
             <Contador />
-        </>
+            <p>Modo: {mode}</p>
+            <p>API url: {import.meta.env.VITE_API_URL}</p>
+        </div>
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const getOutputDir = (mode: string) => {
+  if (mode === "production") return "dist-prod"
+  if (mode === "stg") return "dist-stg"
+  return "dist"
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  build: {
+    outDir: getOutputDir(mode)
+  }
+}))
