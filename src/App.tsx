518e7a26db32dea6f122f03b3d9505dc27b56096
gorkavillar <gorka@gorkavillar.com>
2023-07-13 11:23:50 +0200

    Tema 12 - Contextos y renderizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.scss"
 import Contador from "./views/Contador/Contador"
 
+const contextoUsuario = {
+    usuario: "Brais",
+    isLoggedIn: true,
+}
+const contextoAjustes = {
+    ajustes: {}
+}
+
 function App() {
     return (
         <>
             <Contador />
+            <>
+                <div className="header"></div>
+                <div className="body"></div>
+                <div className="footer"></div>
+            </>
         </>
     )
 }
